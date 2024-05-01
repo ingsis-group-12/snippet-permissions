78cacac326156ab260a0f1e29f8c765f0a9c5efb
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2024-05-01 19:33:51 -0300

    first commit

---
+++ src/main/kotlin/ingsis/group12/snippetpermissons/SnippetPermissonsApplication.kt
@@ -0,0 +1,11 @@
+package ingsis.group12.snippetpermissons
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnippetPermissonsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SnippetPermissonsApplication>(*args)
+}
---
+++ src/test/kotlin/ingsis/group12/snippetpermissons/SnippetPermissonsApplicationTests.kt
@@ -0,0 +1,13 @@
+package ingsis.group12.snippetpermissons
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnippetPermissonsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
