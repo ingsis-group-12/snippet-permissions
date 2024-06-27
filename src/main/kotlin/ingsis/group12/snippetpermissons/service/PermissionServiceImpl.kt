0ce664639875792130994edf9435bd546316ace1
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2024-06-27 04:11:12 -0300

    feat: add new relic with logs

--- src/main/kotlin/ingsis/group12/snippetpermissons/service/PermissionServiceImpl.kt
+++ src/main/kotlin/ingsis/group12/snippetpermissons/service/PermissionServiceImpl.kt
@@ -7,6 +7,7 @@ import ingsis.group12.snippetpermissons.model.Permission
 import ingsis.group12.snippetpermissons.model.PermissionType
 import ingsis.group12.snippetpermissons.model.UserWithoutPermission
 import ingsis.group12.snippetpermissons.repository.PermissionRepository
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.util.UUID
@@ -15,16 +16,22 @@ import java.util.UUID
 class PermissionServiceImpl(
     @Autowired private val permissionRepository: PermissionRepository,
 ) : PermissionService {
+    private val logger = LoggerFactory.getLogger(PermissionServiceImpl::class.java)
     override fun createPermission(
         userId: String,
         snippetId: UUID,
         permissionInput: PermissionInput,
     ): Permission {
+        logger.info("Creating permission for user $userId and snippet $snippetId")
         val existingPermission = permissionRepository.findByUserIdAndSnippetId(userId, snippetId)
+        logger.info("Permission found for user $userId and snippet $snippetId")
         if (existingPermission !== null) {
+            logger.error("Permission already exists for user $userId and snippet $snippetId")
             throw PermissionAlreadyExistsException()
         }
+        logger.info("Creating new permission for user $userId and snippet $snippetId")
         val newPermission = Permission(userId, snippetId, permissionInput.permission, userName = permissionInput.userName)
+        logger.info("Permission created for user $userId and snippet $snippetId")
         return permissionRepository.save(newPermission)
     }
 
@@ -32,10 +39,12 @@ class PermissionServiceImpl(
         snippetId: UUID,
         userId: String,
     ): Permission {
+        logger.info("Getting permission for user $userId and snippet $snippetId")
         return permissionRepository.findByUserIdAndSnippetId(userId, snippetId) ?: throw PermissionNotFoundException()
     }
 
     override fun getPermissionsByUserId(userId: String): List<Permission> {
+        logger.info("Getting permissions for user $userId")
         return permissionRepository.findAllByUserId(userId)
     }
 
@@ -44,7 +53,9 @@ class PermissionServiceImpl(
         snippetId: UUID,
         permission: PermissionType,
     ): Permission {
+        logger.info("Updating permission for user $userId and snippet $snippetId")
         val existingPermission = permissionRepository.findByUserIdAndSnippetId(userId, snippetId) ?: throw PermissionNotFoundException()
+        logger.info("Permission found for user $userId and snippet $snippetId")
         return permissionRepository.updateById(existingPermission.id, permission)
     }
 
@@ -52,6 +63,7 @@ class PermissionServiceImpl(
         snippetId: UUID,
         userId: String,
     ): List<UserWithoutPermission> {
+        logger.info("Getting users who do not have permission for snippet $snippetId")
         val existingPermissions = permissionRepository.findAll()
 
         val usersWithoutPermission = existingPermissions.filter { it.snippetId != snippetId && it.userId != userId }
@@ -64,6 +76,7 @@ class PermissionServiceImpl(
     }
 
     override fun deletePermissionsBySnippetId(snippetId: UUID) {
+        logger.info("Deleting permissions for snippet $snippetId")
         val existingPermissions = permissionRepository.findAllBySnippetId(snippetId)
         permissionRepository.deleteAll(existingPermissions)
     }
